586837aae35a2878a6640dec3d2e12dc4a6cfcc7
OmarFazli <mdomar.fazli@gmail.com>
2025-08-06 17:51:59 -0400

    installed nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.get("/api/notes", (req,res) => {
+    res.send("u sent an API request")
+})
+
 app.listen(3000, ()=> {
     console.log("Server started on port 3000");
-})
+});
