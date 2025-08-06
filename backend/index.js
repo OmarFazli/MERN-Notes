978344a4d168b061b0731c00566e749d1713be2d
OmarFazli <mdomar.fazli@gmail.com>
2025-08-06 18:02:09 -0400

    added dummy routes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,21 @@ const app = express()
 
 app.get("/api/notes", (req,res) => {
     res.send("u sent an API request")
-})
+});
+
+app.post("/api/notes", (req,res) => {
+    res.status(201).json({message:"post created successfully"})
+});
+
+
+app.put("/api/notes/:id", (req,res) => {
+    res.status(200).json({message:"post updated successfully"})
+});
+
+
+app.delete("/api/notes/:id", (req,res) => {
+    res.status(200).json({message:"post deleted successfully"})
+});
 
 app.listen(3000, ()=> {
     console.log("Server started on port 3000");
