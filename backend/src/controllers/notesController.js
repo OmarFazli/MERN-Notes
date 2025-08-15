90de338aadc0c5dad5b1913082cb95b30021c235
OmarFazli <mdomar.fazli@gmail.com>
2025-08-15 18:10:56 -0400

    added sorting to getAllNotes

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,8 +1,8 @@
 import Note from "../model/Note.js";
 
-export const getAllNotes = async (req,res) => {
+export const getAllNotes = async (_,res) => {
     try{
-        const notes = await Note.find();
+        const notes = await Note.find().sort({createdAt:-1});
 
         res.status(200).json(notes);
     } catch (error){
