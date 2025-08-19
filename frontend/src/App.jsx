d44ece9cf1c748948ad3b873c2f3df44f4f60fdb
OmarFazli <mdomar.fazli@gmail.com>
2025-08-19 15:22:20 -0400

    added react-hot-toast boilerplate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from './pages/HomePage'
 import { Route, Routes } from 'react-router'
 import CreatePage from './pages/CreatePage'
 import NoteDetailsPage from './pages/NoteDetailsPage'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success("Congrats")}>Click Me</button>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>}/>
