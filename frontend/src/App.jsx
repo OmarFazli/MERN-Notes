7ed6a8a72eab8d290fd8e2f732c88f12d5959029
OmarFazli <mdomar.fazli@gmail.com>
2025-08-20 14:45:39 -0400

    added daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success("Congrats")} className="text-red-300 p-4 bg-pink-300">Click Me</button>
+    <div data-theme="dark">
+      <button onClick={() => toast.success("Congrats")} className="btn btn-primary">Click Me</button>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>}/>
