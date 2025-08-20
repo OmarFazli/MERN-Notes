cdfcfef916efc53d6b69171aee404e275d6c75c7
OmarFazli <mdomar.fazli@gmail.com>
2025-08-20 17:42:25 -0400

    added conditional rendering of notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -19,14 +19,18 @@ const HomePage = () => {
       try{
         const res = await axios.get('http://localhost:3000/api/notes');
         setNotes(res.data);
+        console.log("Fetched notes: ", res.data);
         setIsLoading(false);
         setIsRateLimited(false);
       } catch(error){
-        if (error.response.status === 429) {
+        if (error.response?.status === 429) {
           handleRateLimit();
           return;
+        } else{
+          toast.error("Failed to fetch notes. Please try again later.");
         }
-        console.error("Error fetching notes:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -36,8 +40,18 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <NavBar/>
-      <button onClick={rateLimitError} className='btn btn-ghost'>press</button>
-      
+
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {isLoading && <div className='text-center mt-10 text-lg'>Loading...</div>}
+        {!isLoading && notes.length === 0 && <div className='text-center text-primary'> No Notes to show</div>}
+        {!isLoading && notes.length > 0 && (
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-5'>
+            {notes.map((note) => (
+              <div>{note.title} | {note.body} </div>
+            ))}
+          </div>)
+        }
+      </div>
     </div>
   )
 }
