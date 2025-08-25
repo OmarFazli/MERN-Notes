8ef7b6a599264677ab346896693db0a98cf78e6f
OmarFazli <mdomar.fazli@gmail.com>
2025-08-25 13:44:30 -0400

    added cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import notesRouter  from "./routes/routes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
+
 const app = express();
-
-
-
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use((req,res,next) => {
     console.log(`Request made to ${req.url} using method ${req.method}`);
     next();
 });
-app.use(rateLimiter);
+
 app.use("/api/notes", notesRouter);
 
 connectDB().then(() => {
