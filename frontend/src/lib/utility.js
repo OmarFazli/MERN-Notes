fa986f01431da3635857c77c1b87b382e44055da
OmarFazli <mdomar.fazli@gmail.com>
2025-09-23 16:33:12 -0400

    changed gitignore file to global

--- frontend/src/lib/utility.js
+++ frontend/src/lib/utility.js
@@ -1,17 +1,25 @@
 import toast from 'react-hot-toast';
 import api from './axios.js';
 
-const userLocale = navigator.language || 'fr-FR';
+// Get user's locale for date formatting, default to 'en-CA' if not available
+const userLocale = navigator.language || 'en-CA';
 const formatDateTime = (datetime) => {
     const time = datetime.toLocaleTimeString(userLocale, { hour: 'numeric', minute: '2-digit', hour12: true });
     const date = datetime.toLocaleDateString(userLocale, { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' });
     return `${time} · ${date}`;
 }
 
+// Utility function to notify user of rate limiting
 const rateLimitError = () => {
     toast.error("You are rate limited. Please try again later.");
 }
 
+// Utility function to notify user of 404 errors
+const notFoundError = () => {
+    toast.error("The requested resource was not found.");
+}
+
+// Utility function to handle note deletion
 const handleNoteDelete = async (e, id) => {
     e.preventDefault();
         
@@ -37,6 +45,7 @@ const handleNoteDelete = async (e, id) => {
     }
 };
 
+// Utility function to handle note update
 const handleNoteUpdate = async (e, id, title, body) => {
     e.preventDefault();
     // do note update here
@@ -45,3 +54,4 @@ const handleNoteUpdate = async (e, id, title, body) => {
 export {formatDateTime};
 export {rateLimitError};
 export {handleNoteDelete};
+export {notFoundError};
